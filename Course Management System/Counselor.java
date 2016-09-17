e5b74727ac44c59049c59c98c6eec0817194dce7
matthewmcdermott <mcdermott1@mail.usf.edu>
2016-09-17 08:49:36 -0400

    Create program functionality for counselor

--- Course Management System/Counselor.java
+++ Course Management System/Counselor.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
 
 public class Counselor extends Person{
+	
+	ArrayList<Student> studentList = new ArrayList<Student>();
 
 	//constructor
 	public Counselor(String id, String n, int num, String add){
@@ -9,11 +12,32 @@ public class Counselor extends Person{
 		setAddress(add);
 	}
 	
-	public Program createProgram(){
+	
+	public boolean addProgramToStudent(Student student, Program program){
+		if (this.studentList.contains(student)){
+			student.setProgram(program);
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public Program createProgram(Student student, ArrayList<Course> courses){
+		
+		//Create the program
 		Program studentProgram = new Program();
-		
-		
-		
+		for (Course course : courses){
+			studentProgram.addCourse(course);
+		}
 		return studentProgram;
+	}
+	
+	public void addStudent(Student s){
+		studentList.add(s);
+	}
+	
+	public void removeStudent(Student s){
+		studentList.remove(s);
 	}
 }
